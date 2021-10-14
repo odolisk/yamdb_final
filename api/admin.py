27b22d98b571976ade6816baedf5f7989628e466
Дмитров Артем <odolisk@yandex.ru>
2021-10-14 11:42:42 +0300

    Fix admin.py import2

--- api/admin.py
+++ api/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import Group
 
-from . models import Category, Comment, Genre, Title, Review, User
+from api.models import Category, Comment, Genre, Title, Review, User
 
 
 class UserAdmin(BaseUserAdmin):
