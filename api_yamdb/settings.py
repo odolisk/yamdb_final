e9d13530f47ff46f2fa6c45a86bea723168fdea6
Дмитров Артем <odolisk@yandex.ru>
2021-10-20 15:04:33 +0300

    Temp vlue for secret key

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'temp_value')
 
 DEBUG = False
 
